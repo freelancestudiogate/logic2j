044335df85bb06ecc3dea7c74e074796420e6502
Laurent Tettoni <laurent.tettoni@gmail.com>
2013-09-24 20:21:18 +0200

    No getters needed

--- src/main/java/org/logic2j/core/library/mgmt/PrimitiveInfo.java
+++ src/main/java/org/logic2j/core/library/mgmt/PrimitiveInfo.java
@@ -60,7 +60,7 @@ public class PrimitiveInfo {
     private final String name;
     private final PLibrary library; // The library instance on which the method will be invoked (they are not static methods)
     private final Method method; // The method that implements the primitive's logic
-    private final boolean varargs;
+    private final boolean isVarargs;
 
     public PrimitiveInfo(PrimitiveType theType, PLibrary theLibrary, String theName, Method theMethod, boolean theVarargs) {
         super();
@@ -68,7 +68,7 @@ public class PrimitiveInfo {
         this.library = theLibrary;
         this.name = theName;
         this.method = theMethod;
-        this.varargs = theVarargs;
+        this.isVarargs = theVarargs;
     }
 
     /**
@@ -81,12 +81,12 @@ public class PrimitiveInfo {
             logger.debug("PRIMITIVE > invocation of {}", this);
         }
         final int arity = theGoalStruct.getArity();
-        final int nbargs = isVarargs() ? 3 : (2 + arity);
+        final int nbargs = this.isVarargs ? 3 : (2 + arity);
         final Object[] args = new Object[nbargs];
         int i = 0;
         args[i++] = theListener;
         args[i++] = theGoalVars;
-        if (isVarargs()) {
+        if (this.isVarargs) {
             // All arguments as an array
             final Object[] varargArray = new Object[arity];
             int j = 0;
@@ -103,7 +103,7 @@ public class PrimitiveInfo {
             }
         }
         try {
-            final Object result = getMethod().invoke(getLibrary(), args);
+            final Object result = this.method.invoke(this.library, args);
             if (debug) {
                 logger.debug("PRIMITIVE < result={}", result);
             }
@@ -111,7 +111,7 @@ public class PrimitiveInfo {
         } catch (final IllegalArgumentException e) {
             throw e;
         } catch (final IllegalAccessException e) {
-            throw new InvalidTermException("Could not access method " + getMethod(), e);
+            throw new InvalidTermException("Could not access method " + this.method, e);
         } catch (final InvocationTargetException e) {
             final Throwable targetException = e.getTargetException();
             if (targetException instanceof RecursionException) {
@@ -138,29 +138,13 @@ public class PrimitiveInfo {
         return this.type;
     }
 
-    public PLibrary getLibrary() {
-        return this.library;
-    }
-
-    public Method getMethod() {
-        return this.method;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public boolean isVarargs() {
-        return this.varargs;
-    }
-
     // ---------------------------------------------------------------------------
     // Methods of java.lang.Object
     // ---------------------------------------------------------------------------
 
     @Override
     public String toString() {
-        return this.getClass().getSimpleName() + "{lib=" + getLibrary() + ", type=" + getType() + ", name=" + getName() + ", method=" + getMethod().getName() + '}';
+        return this.getClass().getSimpleName() + "{lib=" + this.library + ", type=" + getType() + ", name=" + this.name + ", method=" + this.method.getName() + '}';
     }
 
 }
